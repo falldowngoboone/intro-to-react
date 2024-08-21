7edbd0e5b75e29f3443121870673e6079379667b
Ryan Boone <ryan.boone@yum.com>
2024-08-21 10:11:45 -0500

    chore: Add exercise 07 branch

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,6 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:1415',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
   },
   test: {
     environment: 'jsdom',
